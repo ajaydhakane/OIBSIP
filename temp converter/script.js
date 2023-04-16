b4a8a7343908b85cf5330a004588831d4ee21574
ajaydhakane <118625131+ajaydhakane@users.noreply.github.com>
2023-04-16 11:58:31 +0530

    Add files via upload

---
+++ temp converter/script.js
@@ -0,0 +1,15 @@
+let celsius = document.getElementById("celsius");
+let kelvin = document.getElementById("kelvin");
+
+
+function celTokel(){
+    let output = ( parseFloat(celsius.value) ) +273.15;
+    kelvin.value = parseFloat(output.toFixed(2));
+}
+
+function kelToCel(){
+    let output = ( parseFloat(kelvin.value))-273.15;
+    celsius.value = parseFloat( output.toFixed(2));
+    console.log(output);
+}
+
